b0bb36a8306b5855f3bcbe7cb9d34ff439937d5d
weizhiqimail <weizhiqimail@gmail.com>
2025-04-11 21:12:35 +0800

    添加 标准模板库

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,32 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-class MyPrint {
-public:
-    void operator()(const string &name) const {
-        cout << "name = " << name << endl;
-    }
-};
-
-class MyAdd {
-public:
-    int operator()(int num1, int num2) const {
-        return num1 + num2;
-    }
-};
-
-void test01() {
-    MyPrint myPrint;
-    myPrint("Sherry");
-}
-
-void test02() {
-    MyAdd myAdd;
-    cout << "myAdd(1, 2) = " << myAdd(1, 2) << endl;
-    cout << "匿名调用: " << MyAdd()(1, 4) << endl;
-}
-
 int main() {
-    // test01();
-    test02();
-    return 0;
+    vector<int> v = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+    vector<int> result(v.size());
+
+    int scale = 2;
+    int offset = 10;
+
+    // transform(v.begin(), v.end(), result.begin(), [scale, offset](int x, int &y) -> int {
+    //     cout << "x: " << x << ", y: " << y << endl;
+    //     y = x * scale + offset;
+    //     return y;
+    // });
+
+    transform(v.begin(), v.end(), result.begin(),
+              [scale, offset](int x) -> int {
+                  cout << x << "\t";
+                  return x * scale + offset;; // transform 需要返回值
+              });
+    cout << endl;
+
+    auto it = result.begin();
+    while (it != result.end()) {
+        cout << *it << "\t";
+        ++it;
+    }
 }
-/* 输出:
-myAdd(1, 2) = 3
-匿名调用: 5
-*/
