e48ee557060f474b582264d384b866e5cf209927
weizhiqimail <weizhiqimail@gmail.com>
2025-05-07 12:52:13 +0800

    移除C语言代码

--- main.cpp
+++ main.cpp
@@ -1,20 +1,8 @@
 #include <iostream>
-#include "./CircleModule/Circle.h"
 
 using namespace std;
 
 int main() {
-    // 初始化 Color 对象
-    Color redColor = Color(255, 0, 0);
-    Circle circle1 = Circle(10, 0, 0, redColor);
-    circle1.calcArea();
-    circle1.printInfo();
-
-    Color greenColor = Color(0, 255, 0);
-    Point point = Point(0, 0);
-    Circle circle2 = Circle(20, point, greenColor);
-    circle2.calcArea();
-    circle2.printInfo();
-
-    return 0;
+  cout << "hello cpp" << endl;
+  return 0;
 }
